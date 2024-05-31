1c220fe5ed52e1f3c3951cf73a87fa65b7ca9209
ShampaShaw <shampashaw088@gmail.com>
2024-05-31 20:56:48 +0530

    chore: Add initial project files and configurations

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/header'; 
+import Footer from './components/Footer'; 
+import Body from './components/Body'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Body.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div className='text-3xl font-bold underline'>Body</div>
+  )
+}
+
+export default Body
