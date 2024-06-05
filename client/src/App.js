c8c06e5ffe875680b87b731d1325dda3b6219068
ShampaShaw <shampashaw088@gmail.com>
2024-06-05 23:14:15 +0530

    chore: Update .gitignore and package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Header from './components/header'; 
-import Footer from './components/Footer'; 
 import Body from './components/Body'; 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
       <Body/>
-      <Footer/>
     </div>
   );
 }
